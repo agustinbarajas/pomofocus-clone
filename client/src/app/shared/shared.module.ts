dad8e1e09e8aa6be0d9cae97dc4c0de6f0f8ad1d
Ivan Herrera <hherrerabarragan@gmail.com>
2022-11-13 20:23:28 -0700

    Create light button component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LightButtonComponent } from './components/light-button/light-button.component';
 
 
 
 @NgModule({
-  declarations: [ ],
+  declarations: [
+    LightButtonComponent
+  ],
   imports: [
     CommonModule
   ],
