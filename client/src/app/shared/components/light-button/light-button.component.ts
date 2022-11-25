5131dca1d2bc67791dbf8a309ffb2f01c327279f
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-11-24 22:56:18 -0700

    Add light button inputs

--- client/src/app/shared/components/light-button/light-button.component.ts
+++ client/src/app/shared/components/light-button/light-button.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-light-button',
   templateUrl: './light-button.component.html',
   styleUrls: ['./light-button.component.scss']
 })
-export class LightButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LightButtonComponent {
+  @Input() icon!: string;
+  @Input() text!: string;
 }
