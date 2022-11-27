efeab65b02cf94bd2d645047222d8c314f8ddc2b
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-11-26 21:05:12 -0700

    Add light button output for click event

--- client/src/app/shared/components/light-button/light-button.component.ts
+++ client/src/app/shared/components/light-button/light-button.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-light-button',
   templateUrl: './light-button.component.html',
-  styleUrls: ['./light-button.component.scss']
+  styleUrls: ['./light-button.component.scss'],
 })
 export class LightButtonComponent {
   @Input() icon!: string;
   @Input() text!: string;
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick() {
+    this.clicked.emit();
+  }
 }
