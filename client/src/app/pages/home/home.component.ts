802bc8505b7fa056607cf097f24b706f6aea642e
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-12-05 11:51:10 -0700

    Change static tabs for dynamic

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Tab } from 'src/app/shared/interfaces/tab.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+  @ViewChild('pomodoroTemplate') pomodoroRef!: TemplateRef<any>;
+  @ViewChild('shortBreakTemplate') shortRef!: TemplateRef<any>;
+  @ViewChild('longBreakTemplate') longRef!: TemplateRef<any>;
+  tabs: Tab[] = [
+    {
+      name: 'Pomodoro',
+      contentRef: this.pomodoroRef,
+      color: '',
+      isActive: true,
+    },
+    {
+      name: 'Short break',
+      contentRef: this.shortRef,
+      color: '',
+    },
+    {
+      name: 'Long break',
+      contentRef: this.longRef,
+      color: '',
+    },
+  ];
 }
