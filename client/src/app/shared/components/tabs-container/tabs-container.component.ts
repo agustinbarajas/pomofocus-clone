9ca64e101eed804dd3a62e2c96ca0f0026cedffc
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-12-06 11:55:04 -0700

    Add change tab detection

--- client/src/app/shared/components/tabs-container/tabs-container.component.ts
+++ client/src/app/shared/components/tabs-container/tabs-container.component.ts
@@ -1,11 +1,32 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { Tab } from '../../interfaces/tab.interface';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabsContainerComponent {
   @Input() tabs!: Tab[];
+  @Output() selectTab = new EventEmitter<Tab>();
+
+  changeTab(tab: Tab) {
+    this.tabs = this.tabs.map(({ id, isActive, ...rest }) => ({
+      id,
+      ...rest,
+      isActive: tab.id === id,
+    }));
+    this.selectTab.emit(tab);
+  }
+
+  trackCurrentTab(index: number, tab: Tab) {
+    return tab.id;
+  }
 }
