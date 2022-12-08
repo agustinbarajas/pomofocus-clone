1e993bc6af5281cefb567c1cb1b78325c37fd67c
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-12-08 11:27:28 -0700

    Create timer component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LightButtonComponent } from './components/light-button/light-button.component';
 import { TabsContainerComponent } from './components/tabs-container/tabs-container.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
-  declarations: [
-    LightButtonComponent,
-    TabsContainerComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    LightButtonComponent,
-    TabsContainerComponent
-  ]
+  declarations: [LightButtonComponent, TabsContainerComponent, TimerComponent],
+  imports: [CommonModule],
+  exports: [LightButtonComponent, TabsContainerComponent, TimerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
