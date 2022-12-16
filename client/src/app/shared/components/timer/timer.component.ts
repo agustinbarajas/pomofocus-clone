c9a6efe5550138060c3e901df6be37ded54f979a
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-12-16 16:45:06 -0700

    Add timer events

--- client/src/app/shared/components/timer/timer.component.ts
+++ client/src/app/shared/components/timer/timer.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss'],
 })
-export class TimerComponent {}
+export class TimerComponent implements OnInit {
+  @Input() minutes!: number;
+  @Input() textButton = 'Start';
+  @Output() timerStarted = new EventEmitter<void>();
+  @Output() timerPaused = new EventEmitter<void>();
+  @Output() timerEnded = new EventEmitter<void>();
+  @Output() timerSkipped = new EventEmitter<void>();
+  isPressed = false;
+  remainMinutes = 0;
+
+  ngOnInit() {
+    this.remainMinutes = this.minutes;
+  }
+
+  startTimer() {}
+
+  skipTimer() {}
+}
